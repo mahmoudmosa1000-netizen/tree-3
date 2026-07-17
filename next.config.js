32b0d37bd8813edda60322b38d27979fd25b2de6
mahmoudmosa1000-netizen <mahmoudmosa1000@gmail.com>
2026-07-17 21:30:06 +0200

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { typedRoutes: false },
-  images: {
-    domains: ['upload.wikimedia.org', 'commons.wikimedia.org'],
-  },
-};
-
-module.exports = nextConfig;
